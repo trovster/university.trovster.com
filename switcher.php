42644ee87f525cb9f602e310a3fd0ca2126d932b
Trevor Morris <code@trovster.com>
2021-12-01 15:53:12 +0000

    chore: refactor to switcher php

--- switcher.php
+++ switcher.php
@@ -1,17 +1,14 @@
 <?php
-if (isset($_GET['set']))
-{
+if (isset($_GET['set'])) {
 	$cookie_set = $_GET['set'];
-	setcookie('css_style', $cookie_set, time()+31536000);
+	setcookie('css_style', $cookie_set, time() + 31536000);
 }
 
-if($_SERVER['HTTP_REFERER'] && !stristr($_SERVER['HTTP_REFERER'],'presentation'))
-{
+if ($_SERVER['HTTP_REFERER'] && !stristr($_SERVER['HTTP_REFERER'], 'presentation')) {
 	$referred = $_SERVER['HTTP_REFERER'];
 	header("Location: $referred");
 }
-else
-{
+else {
 	$url = 'http://'.$_SERVER['HTTP_HOST'];
 	header("Location: $url");
 }
